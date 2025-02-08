470c28e70bf0ebf682a81c8306ccc8d095d044bf
rimrim990 <lim990718@naver.com>
2025-02-08 23:14:11 +0900

    add theme toggle

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import DarkModeToggle from '@/components/header/DarkModeToggle'
+
+export default function Header() {
+  return (
+    <div>
+      <DarkModeToggle />
+    </div>
+  )
+}
