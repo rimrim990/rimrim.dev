27795387268dbd806f9d9fcde9136bf583ff3534
rimrim990 <lim990718@naver.com>
2025-03-29 15:03:24 +0900

    config mdx prebuild

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,7 +1,16 @@
 import dynamic from "next/dynamic";
+import { promises as fs } from 'fs'
+import path from "path";
 
 interface Props {
     params: Promise<{ id: string }>
+}
+
+export async function generateStaticParams() {
+    const postIds = await fs.readdir(path.join(process.cwd(), 'src', 'markdown'))
+    return postIds.map(id => ({
+        id
+    }))
 }
 
 export default async function Post({params}: Props) {
