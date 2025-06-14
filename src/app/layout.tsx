ac75c91576748fe7099c73abb1870315cad5d87a
rimrim990 <lim990718@naver.com>
2025-06-14 23:40:13 +0900

    delete unused components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from 'next'
 import {Geist, Geist_Mono} from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header/Header'
-import {ThemeProvider} from '@/components/theme-provider'
 import Footer from "@/components/Footer";
 import Sidebar from "@/components/Sidebar";
 
@@ -29,19 +28,12 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-        >
             <Header/>
             <main className="flex gap-8 p-8 flex-col md:flex-row">
                 {children}
                 <Sidebar/>
             </main>
             <Footer/>
-        </ThemeProvider>
         </body>
         </html>
     )
--- src/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface Props {
-  children: React.ReactNode
-}
-
-export default function Title({ children }: Props) {
-  return <h3 className="text-3xl font-bold mt-20 mb-5">{children}</h3>
-}
